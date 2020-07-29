41af25a3ff3ad33cce2d9d4cc4cf0e95f1ffbe1d
Emilsone <lawsonezekiel68@gmail.com>
2020-07-29 15:11:45 +0100

    Created a vue component

---
+++ Wejapa-Task/src/components/MyName.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="name">
+  <h1>{{name}}</h1>
+</div>
+</template>
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name: String
+  }  
+}
+</script>
+<style>
+h1{
+  text-align: center;
+  font-size: 60px;
+  font-weight: 900;
+}
+</style>
+
